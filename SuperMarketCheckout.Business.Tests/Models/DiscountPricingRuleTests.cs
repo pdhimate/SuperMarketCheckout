058608fbc9b0bee1440466eaede73e5e79f4704b
Piyush Dhimate <bitartist@hotmail.com>
2019-04-12 08:35:53 +0530

    Implemented DiscountedPricingRule and added/updated unit tests for it.

--- SuperMarketCheckout.Business.Tests/Models/DiscountPricingRuleTests.cs
+++ SuperMarketCheckout.Business.Tests/Models/DiscountPricingRuleTests.cs
@@ -9,13 +9,67 @@ namespace SuperMarketCheckout.Business.Tests.Models
     [TestClass]
     public class DiscountPricingRuleTests
     {
+        #region NewDiscountPricingRule
+
         [TestMethod]
-        public void ApplyPrice_Throws()
+        public void NewDiscountPricingRule_For0Units_ThrowsArgumentOutOfRangeException()
         {
-            var units = 2;
+            var units = 0;
             var discountPrice = 15;
-            var discountPricing = new DiscountPricingRule(units, discountPrice);
-            Assert.ThrowsException<NotImplementedException>(() => discountPricing.ApplyPrice(null));
+            var regularPricingRule = new RegularPricingRule();
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() =>
+                new DiscountPricingRule(units, discountPrice, regularPricingRule));
         }
+
+        [TestMethod]
+        public void NewDiscountPricingRule_For0DiscountPrice_ThrowsArgumentOutOfRangeException()
+        {
+            var units = 1;
+            var discountPrice = 0;
+            var regularPricingRule = new RegularPricingRule();
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() =>
+                new DiscountPricingRule(units, discountPrice, regularPricingRule));
+        }
+
+        [TestMethod]
+        public void NewDiscountPricingRule_ForNullRegularPricingRule_ThrowsArgumentNulLException()
+        {
+            var units = 1;
+            var discountPrice = 10;
+
+            Assert.ThrowsException<ArgumentNullException>(() =>
+                new DiscountPricingRule(units, discountPrice, null));
+        }
+
+        #endregion
+        #region ApplyPrice
+
+        [TestMethod]
+        public void ApplyPrice_For()
+        {
+            // Init
+            var units = 3;
+            var discountPrice = 25;
+            var regularPricingRule = new RegularPricingRule();
+            var discountPricingRule = new DiscountPricingRule(units, discountPrice, regularPricingRule);
+            var orderItem = new OrderItem
+            {
+                Product = new Product
+                {
+                    UnitPrice = 10
+                },
+                Units = 4
+            };
+
+            // Call
+            var price = discountPricingRule.ApplyPrice(orderItem);
+
+            // Assert
+            Assert.AreEqual(35, price);
+        }
+
+        #endregion
     }
 }
--- SuperMarketCheckout.Business/Models/DiscountPricingRule.cs
+++ SuperMarketCheckout.Business/Models/DiscountPricingRule.cs
@@ -6,23 +6,61 @@ namespace SuperMarketCheckout.Business.Models
 {
     public class DiscountPricingRule : IPricingRule
     {
-        private int _units;
+        private readonly int _threshholdUnits;
 
         /// <summary>
-        /// The offer/discounted price for the number of <see cref="_units"/> purchased
+        /// The offer/discounted price for the number of <see cref="_threshholdUnits"/> purchased
         /// </summary>
 
-        private decimal _discountPrice;
+        private readonly decimal _discountPrice;
 
-        public DiscountPricingRule(int units, decimal discountPrice)
+        private readonly IPricingRule _regularPricingRule;
+
+        public DiscountPricingRule(int threshholdUnits, decimal discountPrice,
+            IPricingRule regularPricingRule)
         {
-            _units = units;
+            //Validations
+            if (threshholdUnits < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(threshholdUnits));
+            }
+            if (discountPrice < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(discountPrice));
+            }
+            if (regularPricingRule == null)
+            {
+                throw new ArgumentNullException(nameof(regularPricingRule));
+            }
+
+            // Init fields
+            _threshholdUnits = threshholdUnits;
             _discountPrice = discountPrice;
+            _regularPricingRule = regularPricingRule;
         }
 
         public decimal ApplyPrice(OrderItem orderItem)
         {
-            throw new NotImplementedException();
+            if (orderItem == null)
+            {
+                throw new ArgumentNullException(nameof(orderItem));
+            }
+
+            if (orderItem.Units >= _threshholdUnits)
+            {
+                // Calculate price for discounted items
+                var discountedUnitsGroup = orderItem.Units / _threshholdUnits;
+                var discountedTotal = discountedUnitsGroup * _discountPrice;
+
+                // Calculate price for remaining items, if any
+                var discountedUnits = discountedUnitsGroup * _threshholdUnits;
+                var remainingUnits = orderItem.Units - discountedUnits;
+                var regularTotal = remainingUnits * orderItem.Product.UnitPrice;
+
+                return discountedTotal + regularTotal;
+            }
+
+            return _regularPricingRule.ApplyPrice(orderItem);
         }
     }
 }
