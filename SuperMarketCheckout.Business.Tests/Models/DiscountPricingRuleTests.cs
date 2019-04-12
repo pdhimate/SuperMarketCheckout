925e34c36445e25bc06fcbf831bec28ad2f29e79
Piyush Dhimate <bitartist@hotmail.com>
2019-04-12 08:07:45 +0530

    Added DiscountPricingRule and Unit tests for it.

---
+++ SuperMarketCheckout.Business.Tests/Models/DiscountPricingRuleTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SuperMarketCheckout.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperMarketCheckout.Business.Tests.Models
+{
+    [TestClass]
+    public class DiscountPricingRuleTests
+    {
+        [TestMethod]
+        public void ApplyPrice_Throws()
+        {
+            var discountPricing = new DiscountPricingRule();
+            Assert.ThrowsException<NotImplementedException>(() => discountPricing.ApplyPrice(null));
+        }
+    }
+}
