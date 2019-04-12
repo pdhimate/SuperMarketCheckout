9395d0d319e6db6f49a49f0dcbb571b3ad486d4d
Piyush Dhimate <bitartist@hotmail.com>
2019-04-12 08:50:50 +0530

    Implemented BillingRegister to calculate cart total with appropriate discount rules being applied. Added a positive case Test for it.

---
+++ SuperMarketCheckout.Business.Tests/BillingRegisterTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SuperMarketCheckout.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperMarketCheckout.Business.Tests
+{
+    [TestClass]
+    public class BillingRegisterTests
+    {
+        [TestMethod]
+        public void Checkout()
+        {
+            var product1 = new Product
+            {
+                UnitPrice = 10,
+                PricingRules = new List<IPricingRule>
+                {
+                    new RegularPricingRule()
+                }
+            };
+            var regPricingRule = new RegularPricingRule();
+            var product2 = new Product
+            {
+                UnitPrice = 20,
+                PricingRules = new List<IPricingRule>
+                {
+                    regPricingRule,
+                    new DiscountPricingRule(2, 35, regPricingRule)
+                }
+            };
+
+            var cartItems = new List<OrderItem>
+            {
+                new OrderItem
+                {
+                    Product = product1,
+                    Units = 2
+                },
+                new OrderItem
+                {
+                    Product = product2,
+                    Units= 3
+                }
+            };
+
+
+            var register = new BillingRegister();
+            var total = register.Checkout(cartItems);
+
+            Assert.AreEqual(75, total);
+        }
+    }
+}
--- SuperMarketCheckout.Business/BillingRegister.cs
+++ SuperMarketCheckout.Business/BillingRegister.cs
@@ -1,14 +1,32 @@
-﻿using System;
+﻿using SuperMarketCheckout.Business.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SuperMarketCheckout.Business
 {
-    class BillingRegister
+    public class BillingRegister
     {
-        void Checkout()
+        public decimal Checkout(ICollection<OrderItem> orderItems)
         {
+            decimal cartTotal = 0;
+            foreach (var orderItem in orderItems)
+            {
+                var lowestPrice = decimal.MaxValue;
+                foreach (var rule in orderItem.Product.PricingRules)
+                {
+                    var price = rule.ApplyPrice(orderItem);
+                    if (price < lowestPrice)
+                    {
+                        lowestPrice = price;
+                    }
+                }
 
+                orderItem.Total = lowestPrice;
+                cartTotal += lowestPrice;
+            }
+
+            return cartTotal;
         }
     }
 }
